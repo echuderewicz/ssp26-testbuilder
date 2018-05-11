e54c6d54bb98ab4b4ade6d6c25740000e473a121
eric chuderewicz <echuderewicz@gmail.com>
2018-05-10 23:38:30 -0400

    now working, I was using wrong file

--- detectNetwork.js
+++ detectNetwork.js
@@ -21,36 +21,9 @@ function getPrefix(num){
     return num.substring(0,2);
   }
 }
-
-var detectNetworkOld = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
   
-
-  if(cardNumber.length === 14 && (getPrefix(cardNumber) === '38' || getPrefix(cardNumber) === '39')){
-  	return "Diner's Club";
-  }else if(cardNumber.length === 15 && (getPrefix(cardNumber) === '34' || getPrefix(cardNumber) === '37')){
-  	return "American Express";
-  }else if((cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19) && getPrefix(cardNumber) === '4'){
-    return "Visa";
-  }else if(cardNumber.length === 16 && 
-    (getPrefix(cardNumber) === '51' ||
-      getPrefix(cardNumber) === '52' ||
-      getPrefix(cardNumber) === '53' ||
-      getPrefix(cardNumber) === '54' ||
-      getPrefix(cardNumber) === '55')){
-    return "Mastercard";
-  }else{
-  	return false;
-  }
-};
-
 function getFirstTwoDigits(cn){
-  return cn.slice(0,1);
-
+  return cn.slice(0,2);
 }
 
 function detectNetwork(cardNumber){
@@ -58,144 +31,28 @@ function detectNetwork(cardNumber){
   let lengthOfCard = cardNumber.length;
 
   var isVisa = (parseInt(firstTwoDigits/10) === 4) && (lengthOfCard === 13 || lengthOfCard === 16 || lengthOfCard === 19);
-  var isMastercard = (parseInt(firstTwoDigits) === 4) && (lengthOfCard === 13 || lengthOfCard === 16 || lengthOfCard === 19);
-  var isDiners = (parseInt(firstTwoDigits) === 4) && (lengthOfCard === 13 || lengthOfCard === 16 || lengthOfCard === 19);
-  var isAmex = (parseInt(firstTwoDigits) === 4) && (lengthOfCard === 13 || lengthOfCard === 16 || lengthOfCard === 19);
+  var isMastercard = (parseInt(firstTwoDigits) === 51 || parseInt(firstTwoDigits) === 52 || 
+                      parseInt(firstTwoDigits) === 53 || parseInt(firstTwoDigits) === 54 || 
+                      parseInt(firstTwoDigits) === 55) && lengthOfCard === 16;
+  var isDiners = (parseInt(firstTwoDigits) === 38 || parseInt(firstTwoDigits) === 39) && lengthOfCard === 14;
+  var isAmex = (parseInt(firstTwoDigits) === 34 || parseInt(firstTwoDigits) === 37) && lengthOfCard === 15;
+
 
   if(isVisa){
     return 'Visa';
+  }else if(isMastercard){
+    return 'Mastercard';
+  }else if(isDiners){
+    return "Diner's Club";
+  }else if(isAmex){
+    return "American Express";
+  }else{
+    return "";
   }
-
-
-
-
 }
 
 //provided to me by Nikshala
 //var isVisa = (parseInt(firstTwoDigits/10) === 4) && (lengthOfCard === 13 || lengthOfCard === 16 || lengthOfCard === 19);
-
-function assertEqual(actual, expected, testName){
-	if(actual === expected){
-		console.log("Passed");
-	}else{
-		console.log("Failed");
-	}
-}
-
-
-// STEP TWO:
-// Nice work! Extend your function to support two popular networks, Visa and Mastercard:
-
-// Visa always has a prefix of 4 and a length of 13, 16, or 19.
-// MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
-
-// Make sure that you continue to support Diner's Club and American Express cards. Keep testing your implementation here in the console.
-
-// Here's a list of card numbers you can try to ensure that your function works for every combination of prefix and length: 
-
-// 38345678901234 (Diner's Club)
-// 39345678901234 (Diner's Club)
-// 343456789012345 (American Express)
-// 373456789012345 (American Express)
-// 4123456789012 (Visa)
-// 4123456789012345 (Visa)
-// 4123456789012345678 (Visa)
-// 5112345678901234 (MasterCard)
-// 5212345678901234 (MasterCard)
-// 5312345678901234 (MasterCard)
-// 5412345678901234 (MasterCard)
-// 5512345678901234 (MasterCard)
-
-let prefixes = [4, 34, 37, 38, 39, 51, 52, 53, 54, 55];
-let lengths = [13, 14, 15, 16, 19];
-
-function turnNumberToString(){
-
-}
-
-let codes = {
-  13: 1000000000000,
-  14: 10000000000000,
-  15: 100000000000000,
-  16: 1000000000000000,
-  19: 1000000000000000000
-}
-
-function generateRandom(prefix, lengthOfNumber){
-  //return prefix;
-  
-  if(prefix.toString().length === 1){
-    let random = Math.floor(codes[lengthOfNumber] + Math.random() * (codes[lengthOfNumber] * 9));
-    let suffix = random.toString().split("");
-
-    suffix.shift();
-
-    return prefix + suffix.join("");
-
-  }else{
-    let random = Math.floor(codes[lengthOfNumber] + Math.random() * (codes[lengthOfNumber] * 9));
-    let suffix = random.toString().split("");
-
-    suffix.shift();
-    suffix.shift();
-
-    return prefix + suffix.join("");
-
-  }
-  
-}
-
-function getCombinationsArray(p,l){
-  //let count = 0;
-  let combinationArray = [];
-  let prefixes = p;
-  let lengths = l;
-
-  prefixes.forEach(function(pre){
-    lengths.forEach(function(length){
-      combinationArray.push(generateRandom(pre, length))
-    })
-  })
-  return combinationArray;
-}
-
-let combos = getCombinationsArray(prefixes, lengths);
-
-function runCombos(combinations){
-  let network = [];
-
-  combinations.forEach(function(val){
-    network.push(detectNetwork(val));
-
-  })
-
-  return network;
-}
-
-function run(){
-  let res = runCombos(combos);
-  return res;
-}
-
-// combos.forEach(function(val, index){
-//   console.log("Test No. " + (index + 1) + " " + val + " length: " + val.length + " network: " + detectNetwork(val));
-// })
-
-
-// function runAgain(){
-//   detectNetwork("38345678901234");
-//   detectNetwork("39345678901234");
-//   detectNetwork("343456789012345");
-//   detectNetwork("373456789012345");
-//   detectNetwork("4123456789012");
-//   detectNetwork("4123456789012345");
-//   detectNetwork("4123456789012345678");
-//   detectNetwork("5112345678901234");
-//   detectNetwork("5212345678901234");
-//   detectNetwork("5312345678901234");
-//   detectNetwork("5412345678901234");
-//   detectNetwork("5512345678901234");
-// }
 
 function assertEqual(expected, actual, testname){
   if(expected === actual){
@@ -205,32 +62,33 @@ function assertEqual(expected, actual, testname){
   }
 }
 
-assertEqual(detectNetwork("38345678901234"), "Diner's Club", "Checking if Network is correct");
-assertEqual(detectNetwork("39345678901234"), "Diner's Club", "Checking if Network is correct");
-assertEqual(detectNetwork("343456789012345"), "American Express", "Checking if Network is correct");
-assertEqual(detectNetwork("373456789012345"), "American Express", "Checking if Network is correct");
-assertEqual(detectNetwork("4123456789012"), "Visa", "Checking if Network is correct");
-assertEqual(detectNetwork("4123456789012345"), "Visa", "Checking if Network is correct");
-assertEqual(detectNetwork("4123456789012345678"), "Visa", "Checking if Network is correct");
-assertEqual(detectNetwork("5112345678901234"), "Mastercard", "Checking if Network is correct");
-assertEqual(detectNetwork("5212345678901234"), "Mastercard", "Checking if Network is correct");
-assertEqual(detectNetwork("5312345678901234"), "Mastercard", "Checking if Network is correct");
-assertEqual(detectNetwork("5412345678901234"), "Mastercard", "Checking if Network is correct");
-assertEqual(detectNetwork("5512345678901234"), "Mastercard", "Checking if Network is correct");
+console.log(detectNetwork("38345678901234"));
+console.log(detectNetwork("39345678901234"));
+console.log(detectNetwork("343456789012345"));
+console.log(detectNetwork("373456789012345"));
+console.log(detectNetwork("4123456789012"));
+console.log(detectNetwork("4123456789012345"));
+console.log(detectNetwork("4123456789012345678"));
+console.log(detectNetwork("5112345678901234"));
+console.log(detectNetwork("5212345678901234"));
+console.log(detectNetwork("5312345678901234"));
+console.log(detectNetwork("5412345678901234"));
+console.log(detectNetwork("5512345678901234"));
 
 
-// 38345678901234 (Diner's Club)
-// 39345678901234 (Diner's Club)
-// 343456789012345 (American Express)
-// 373456789012345 (American Express)
-// 4123456789012 (Visa)
-// 4123456789012345 (Visa)
-// 4123456789012345678 (Visa)
-// 5112345678901234 (MasterCard)
-// 5212345678901234 (MasterCard)
-// 5312345678901234 (MasterCard)
-// 5412345678901234 (MasterCard)
-// 5512345678901234 (MasterCard)
+// assertEqual(detectNetwork("38345678901234"), "Diner's Club", "Checking if Network is correct");
+// assertEqual(detectNetwork("39345678901234"), "Diner's Club", "Checking if Network is correct");
+// assertEqual(detectNetwork("343456789012345"), "American Express", "Checking if Network is correct");
+// assertEqual(detectNetwork("373456789012345"), "American Express", "Checking if Network is correct");
+// assertEqual(detectNetwork("4123456789012"), "Visa", "Checking if Network is correct");
+// assertEqual(detectNetwork("4123456789012345"), "Visa", "Checking if Network is correct");
+// assertEqual(detectNetwork("4123456789012345678"), "Visa", "Checking if Network is correct");
+// assertEqual(detectNetwork("5112345678901234"), "Mastercard", "Checking if Network is correct");
+// assertEqual(detectNetwork("5212345678901234"), "Mastercard", "Checking if Network is correct");
+// assertEqual(detectNetwork("5312345678901234"), "Mastercard", "Checking if Network is correct");
+// assertEqual(detectNetwork("5412345678901234"), "Mastercard", "Checking if Network is correct");
+// assertEqual(detectNetwork("5512345678901234"), "Mastercard", "Checking if Network is correct");
+
 
 
 
